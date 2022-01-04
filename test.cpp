f46eed85c32f4d1ab99b7a1e6236689d23c59aa1
Muhammad Mustafa <mmustafa.bese20seecs@seecs.edu.pk>
2022-01-05 01:30:42 +0500

    Refactor Code and make Trie a Class Template

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <list>
+#include <stack>
+using namespace std;
+
+
+int main ()
+{
+    stack <int> intlist;
+    intlist.push(9);
+    intlist.push(8);
+    return 0;
+}
